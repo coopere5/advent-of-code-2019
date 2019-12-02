586540cba44f8f26dfb489bb23445d2e32bb5225
Evan Cooper <coopere5@users.noreply.github.com>
2019-12-02 11:06:59 -0500

    Update Program.cs

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -4,7 +4,7 @@ namespace Day2
 {
     class Program
     {
-        private static readonly int[] rom = {
+        private static int[] rom = {
             1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 13, 1, 19, 1, 10, 19, 23, 1, 23, 9, 27, 1, 5, 27, 31, 2,
             31, 13, 35, 1, 35, 5, 39, 1, 39, 5, 43, 2, 13, 43, 47, 2, 47, 10, 51, 1, 51, 6, 55, 2, 55, 9, 59, 1, 59, 5,
             63, 1, 63, 13, 67, 2, 67, 6, 71, 1, 71, 5, 75, 1, 75, 5, 79, 1, 79, 9, 83, 1, 10, 83, 87, 1, 87, 10, 91, 1,
@@ -21,7 +21,8 @@ namespace Day2
 
         private static void Day1()
         {
-            int[] ram = rom;
+            int[] ram = new int[rom.Length];
+            Array.Copy(rom, ram, rom.Length);
 
             int noun = 12;
             int verb = 2;
