9d9c2c0b3a768817a7ea4f1cd334054b45600b19
Evan Cooper <coopere5@users.noreply.github.com>
2019-12-09 10:58:11 -0500

    Update Program.cs

--- Day 9/Program.cs
+++ Day 9/Program.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
+using AdventUtils;
 
 namespace Day9
 {
@@ -70,172 +69,16 @@ namespace Day9
         {
             Console.WriteLine("Input 1 for Part 1");
 
-            var ram = new long[rom.Count];
-            rom.CopyTo(ram, 0);
-            long output;
-            long idx = 0;
-            Run(ram.ToDictionary(key => idx++, value => value), out output);
+            IntcodeComputer computer = new IntcodeComputer(rom);
+            computer.Run();
         }
 
         private static void Part2()
         {
             Console.WriteLine("Input 2 for Part 2");
-            var ram = new long[rom.Count];
-            rom.CopyTo(ram, 0);
-            long output;
-            long idx = 0;
-            Run(ram.ToDictionary(key => idx++, value => value), out output);
-        }
 
-        private static long Run(IDictionary<long, long> intcode, out long output, params long[] inputParams)
-        {
-            output = -1;
-            var inputList = inputParams.ToList();
-            var sw = System.Diagnostics.Stopwatch.StartNew();
-
-            long relativeBase = 0;
-
-            long currentPos = 0;
-            while (true)
-            {
-                string fullOpcode = intcode[currentPos].ToString().PadLeft(5, '0');
-                long opcode = long.Parse(fullOpcode.Substring(3, 2));
-                long param1Mode = long.Parse(fullOpcode.Substring(2, 1));
-                long param2Mode = long.Parse(fullOpcode.Substring(1, 1));
-                long param3Mode = long.Parse(fullOpcode.Substring(0, 1));
-
-                long param1 = 0;
-                long param2 = 0;
-                long param3 = 0;
-
-                long param1Key = 0;
-                long param2Key = 0;
-                long param3Key = 0;
-
-                if (new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }.Contains(opcode))
-                {
-                    if (!intcode.ContainsKey(currentPos + 1)) intcode.Add(currentPos + 1, 0);
-                    switch (param1Mode)
-                    {
-                        case 0:
-                            param1Key = intcode[currentPos + 1];
-                            if (!intcode.ContainsKey(param1Key)) intcode.Add(param1Key, 0);
-                            param1 = intcode[param1Key];
-                            break;
-                        case 1:
-                            param1 = intcode[currentPos + 1];
-                            break;
-                        case 2:
-                            param1Key = relativeBase + intcode[currentPos + 1];
-                            if (!intcode.ContainsKey(param1Key)) intcode.Add(param1Key, 0);
-                            param1 = intcode[param1Key];
-                            break;
-                    }
-                }
-
-                if (new long[] { 1, 2, 5, 6, 7, 8 }.Contains(opcode))
-                {
-                    if (!intcode.ContainsKey(currentPos + 2)) intcode.Add(currentPos + 2, 0);
-                    switch (param2Mode)
-                    {
-                        case 0:
-                            param2Key = intcode[currentPos + 2];
-                            if (!intcode.ContainsKey(param2Key)) intcode.Add(param2Key, 0);
-                            param2 = intcode[param2Key];
-                            break;
-                        case 1:
-                            param2 = intcode[currentPos + 2];
-                            break;
-                        case 2:
-                            param2Key = relativeBase + intcode[currentPos + 2];
-                            if (!intcode.ContainsKey(param2Key)) intcode.Add(param2Key, 0);
-                            param2 = intcode[param2Key];
-                            break;
-                    }
-                }
-
-                if (new long[] { 1, 2, 7, 8 }.Contains(opcode))
-                {
-                    if (!intcode.ContainsKey(currentPos + 3)) intcode.Add(currentPos + 3, 0);
-                    switch (param3Mode)
-                    {
-                        case 0:
-                            param3Key = intcode[currentPos + 3];
-                            if (!intcode.ContainsKey(param3Key)) intcode.Add(param3Key, 0);
-                            param3 = intcode[param3Key];
-                            break;
-                        case 1:
-                            param3 = intcode[currentPos + 3];
-                            break;
-                        case 2:
-                            param3Key = relativeBase + intcode[currentPos + 3];
-                            if (!intcode.ContainsKey(param3Key)) intcode.Add(param3Key, 0);
-                            param3 = intcode[param3Key];
-                            break;
-                    }
-                }
-
-                switch (opcode)
-                {
-                    case 1:
-                        intcode[param3Key] = param1 + param2;
-                        currentPos += 4;
-                        break;
-                    case 2:
-                        intcode[param3Key] = param1 * param2;
-                        currentPos += 4;
-                        break;
-                    case 3:
-                        Console.Write("Input: ");
-                        string input;
-                        if (inputList.Any())
-                        {
-                            input = inputList[0].ToString();
-                            Console.WriteLine(input);
-                            inputList.RemoveAt(0);
-                        }
-                        else
-                        {
-                            sw.Stop();
-                            input = Console.ReadLine();
-                            sw.Start();
-                        }
-                        intcode[param1Key] = int.Parse(input.Trim());
-                        currentPos += 2;
-                        break;
-                    case 4:
-                        Console.WriteLine($"Output: {param1}");
-                        output = param1;
-                        currentPos += 2;
-                        break;
-                    case 5:
-                        if (param1 != 0) currentPos = param2;
-                        else currentPos += 3;
-                        break;
-                    case 6:
-                        if (param1 == 0) currentPos = param2;
-                        else currentPos += 3;
-                        break;
-                    case 7:
-                        intcode[param3Key] = param1 < param2 ? 1 : 0;
-                        currentPos += 4;
-                        break;
-                    case 8:
-                        intcode[param3Key] = param1 == param2 ? 1 : 0;
-                        currentPos += 4;
-                        break;
-                    case 9:
-                        relativeBase += param1;
-                        currentPos += 2;
-                        break;
-                    case 99:
-                        sw.Stop();
-                        System.Diagnostics.Debug.WriteLine(sw.Elapsed);
-                        return intcode[0];
-                    default:
-                        throw new Exception($"Unknown opcode: {opcode} at {currentPos}");
-                }
-            }
+            IntcodeComputer computer = new IntcodeComputer(rom);
+            computer.Run();
         }
     }
 }
