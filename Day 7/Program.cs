45aa0c6db0d4a2d73e976e432fcb0a44871c266a
Evan Cooper <coopere5@users.noreply.github.com>
2019-12-11 15:02:20 -0500

    complete 7-2

--- Day 7/Program.cs
+++ Day 7/Program.cs
@@ -1,206 +1,117 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using AdventUtils;
 
-namespace AdventUtils
+namespace Day7
 {
-    public class IntcodeComputer
+    internal class Program
     {
-        private readonly IDictionary<long, long> Memory;
-        private long RelativeBase;
-        private long InstructionPtr;
+        private static readonly ReadOnlyCollection<long> rom;
 
-        public Queue<long> InputQueue { get; set; }
-        public Queue<long> OutputQueue { get; set; }
-
-        public IntcodeComputer()
+        static Program()
         {
-            RelativeBase = 0;
-            InstructionPtr = 0;
-
-            InputQueue = new Queue<long>();
-            OutputQueue = new Queue<long>();
-        }
-
-        public IntcodeComputer(IEnumerable<long> intcode) : this()
-        {
-            long idx = 0;
-            Memory = intcode.ToDictionary(key => idx++, value => value);
-        }
-
-        public IntcodeComputer(IDictionary<long, long> intcode) : this()
-        {
-            Memory = intcode;
-        }
-
-        public long Run()
-        {
-            return Run(out long output);
-        }
-
-        public long Run(out long output, params long[] inputParams)
-        {
-            long rv = 0;
-            RelativeBase = 0;
-            InstructionPtr = 0;
-            output = 0;
-            Array.ForEach(inputParams, e => InputQueue.Enqueue(e));
-
-            bool running = true;
-            while (running)
+            rom = Array.AsReadOnly(new long[]
             {
-                rv = RunNext();
-                running = rv == long.MinValue;
-            }
-
-            if (OutputQueue.Any()) output = OutputQueue.Dequeue();
-
-            return rv;
+                3, 8, 1001, 8, 10, 8, 105, 1, 0, 0, 21, 30, 51, 76, 101, 118, 199, 280, 361, 442, 99999, 3, 9, 102, 5,
+                9, 9, 4, 9, 99, 3, 9, 102, 4, 9, 9, 1001, 9, 3, 9, 102, 2, 9, 9, 101, 2, 9, 9, 4, 9, 99, 3, 9, 1002, 9,
+                3, 9, 1001, 9, 4, 9, 102, 5, 9, 9, 101, 3, 9, 9, 1002, 9, 3, 9, 4, 9, 99, 3, 9, 101, 5, 9, 9, 102, 4, 9,
+                9, 1001, 9, 3, 9, 1002, 9, 2, 9, 101, 4, 9, 9, 4, 9, 99, 3, 9, 1002, 9, 2, 9, 1001, 9, 3, 9, 102, 5, 9,
+                9, 4, 9, 99, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9,
+                1001, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3,
+                9, 101, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 99, 3, 9, 1001, 9, 1, 9, 4,
+                9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9,
+                4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9,
+                4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 99, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 1,
+                9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101,
+                1, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 99, 3, 9,
+                1001, 9, 1, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3,
+                9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9,
+                3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 99, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9,
+                4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9,
+                4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2,
+                9, 4, 9, 99
+            });
         }
 
-        public long RunNext()
+        private static void Main()
         {
-            string fullOpcode = Memory[InstructionPtr].ToString().PadLeft(5, '0');
-            long opcode = long.Parse(fullOpcode.Substring(3, 2));
-            long param1Mode = long.Parse(fullOpcode.Substring(2, 1));
-            long param2Mode = long.Parse(fullOpcode.Substring(1, 1));
-            long param3Mode = long.Parse(fullOpcode.Substring(0, 1));
+            Part1();
+            Part2();
+            Console.ReadLine();
+        }
 
-            long param1 = 0;
-            long param2 = 0;
-            long param3 = 0;
-
-            long param1Key = 0;
-            long param2Key = 0;
-            long param3Key = 0;
-
-            if (new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }.Contains(opcode))
+        private static void Part1()
+        {
+            IntcodeComputer computer = new IntcodeComputer(rom);
+            long max = int.MinValue;
+            foreach (var settings in GetPermutations(Enumerable.Range(0, 5), 5))
             {
-                if (!Memory.ContainsKey(InstructionPtr + 1)) Memory.Add(InstructionPtr + 1, 0);
-                switch (param1Mode)
+                long signal = 0;
+                foreach (var phase in settings)
                 {
-                    case 0:
-                        param1Key = Memory[InstructionPtr + 1];
-                        if (!Memory.ContainsKey(param1Key)) Memory.Add(param1Key, 0);
-                        param1 = Memory[param1Key];
-                        break;
-                    case 1:
-                        param1 = Memory[InstructionPtr + 1];
-                        break;
-                    case 2:
-                        param1Key = RelativeBase + Memory[InstructionPtr + 1];
-                        if (!Memory.ContainsKey(param1Key)) Memory.Add(param1Key, 0);
-                        param1 = Memory[param1Key];
-                        break;
+                    computer.Run(out signal, phase, signal);
+                    signal = computer.OutputQueue.Dequeue();
                 }
+                max = Math.Max(max, signal);
             }
-
-            if (new long[] { 1, 2, 5, 6, 7, 8 }.Contains(opcode))
-            {
-                if (!Memory.ContainsKey(InstructionPtr + 2)) Memory.Add(InstructionPtr + 2, 0);
-                switch (param2Mode)
-                {
-                    case 0:
-                        param2Key = Memory[InstructionPtr + 2];
-                        if (!Memory.ContainsKey(param2Key)) Memory.Add(param2Key, 0);
-                        param2 = Memory[param2Key];
-                        break;
-                    case 1:
-                        param2 = Memory[InstructionPtr + 2];
-                        break;
-                    case 2:
-                        param2Key = RelativeBase + Memory[InstructionPtr + 2];
-                        if (!Memory.ContainsKey(param2Key)) Memory.Add(param2Key, 0);
-                        param2 = Memory[param2Key];
-                        break;
-                }
-            }
-
-            if (new long[] { 1, 2, 7, 8 }.Contains(opcode))
-            {
-                if (!Memory.ContainsKey(InstructionPtr + 3)) Memory.Add(InstructionPtr + 3, 0);
-                switch (param3Mode)
-                {
-                    case 0:
-                        param3Key = Memory[InstructionPtr + 3];
-                        if (!Memory.ContainsKey(param3Key)) Memory.Add(param3Key, 0);
-                        param3 = Memory[param3Key];
-                        break;
-                    case 1:
-                        param3 = Memory[InstructionPtr + 3];
-                        break;
-                    case 2:
-                        param3Key = RelativeBase + Memory[InstructionPtr + 3];
-                        if (!Memory.ContainsKey(param3Key)) Memory.Add(param3Key, 0);
-                        param3 = Memory[param3Key];
-                        break;
-                }
-            }
-
-            switch (opcode)
-            {
-                case 1:
-                    Memory[param3Key] = param1 + param2;
-                    InstructionPtr += 4;
-                    break;
-                case 2:
-                    Memory[param3Key] = param1 * param2;
-                    InstructionPtr += 4;
-                    break;
-                case 3:
-                    string input;
-                    if (InputQueue.Any())
-                    {
-                        input = InputQueue.Dequeue().ToString();
-                        //Console.WriteLine($"Input: {input}");
-                    }
-                    else
-                    {
-                        Console.Write("Input: ");
-                        input = Console.ReadLine();
-                    }
-                    Memory[param1Key] = int.Parse(input.Trim());
-                    InstructionPtr += 2;
-                    break;
-                case 4:
-                    //Console.WriteLine($"Output: {param1}");
-                    OutputQueue.Enqueue(param1);
-                    InstructionPtr += 2;
-                    break;
-                case 5:
-                    if (param1 != 0) InstructionPtr = param2;
-                    else InstructionPtr += 3;
-                    break;
-                case 6:
-                    if (param1 == 0) InstructionPtr = param2;
-                    else InstructionPtr += 3;
-                    break;
-                case 7:
-                    Memory[param3Key] = param1 < param2 ? 1 : 0;
-                    InstructionPtr += 4;
-                    break;
-                case 8:
-                    Memory[param3Key] = param1 == param2 ? 1 : 0;
-                    InstructionPtr += 4;
-                    break;
-                case 9:
-                    RelativeBase += param1;
-                    InstructionPtr += 2;
-                    break;
-                case 99:
-                    return Memory[0];
-                default:
-                    throw new Exception($"Unknown opcode: {opcode} at {InstructionPtr}");
-            }
-
-            return long.MinValue;
+            Console.WriteLine($"Part 1: {max}");
         }
 
-        public long PeekNext()
+        private static void Part2()
         {
-            string fullOpcode = Memory[InstructionPtr].ToString().PadLeft(5, '0');
-            long opcode = long.Parse(fullOpcode.Substring(3, 2));
-            return opcode;
+            long max = long.MinValue;
+            foreach (var settings in GetPermutations(Enumerable.Range(5, 5), 5))
+            {
+                IntcodeComputer computerA = new IntcodeComputer(rom);
+                IntcodeComputer computerB = new IntcodeComputer(rom);
+                IntcodeComputer computerC = new IntcodeComputer(rom);
+                IntcodeComputer computerD = new IntcodeComputer(rom);
+                IntcodeComputer computerE = new IntcodeComputer(rom);
+
+                computerA.InputQueue = computerE.OutputQueue;
+                computerB.InputQueue = computerA.OutputQueue;
+                computerC.InputQueue = computerB.OutputQueue;
+                computerD.InputQueue = computerC.OutputQueue;
+                computerE.InputQueue = computerD.OutputQueue;
+
+                var settingsArray = settings as int[] ?? settings.ToArray();
+                computerA.InputQueue.Enqueue(settingsArray[0]);
+                computerB.InputQueue.Enqueue(settingsArray[1]);
+                computerC.InputQueue.Enqueue(settingsArray[2]);
+                computerD.InputQueue.Enqueue(settingsArray[3]);
+                computerE.InputQueue.Enqueue(settingsArray[4]);
+
+                computerA.InputQueue.Enqueue(0);
+
+                bool running = true;
+                while (running)
+                {
+                    if (computerA.PeekNext() != 3 || computerA.InputQueue.Any()) computerA.RunNext();
+                    if (computerB.PeekNext() != 3 || computerB.InputQueue.Any()) computerB.RunNext();
+                    if (computerC.PeekNext() != 3 || computerC.InputQueue.Any()) computerC.RunNext();
+                    if (computerD.PeekNext() != 3 || computerD.InputQueue.Any()) computerD.RunNext();
+                    if (computerE.PeekNext() != 3 || computerE.InputQueue.Any()) running = computerE.RunNext() == long.MinValue;
+                }
+
+                long signal = computerE.OutputQueue.Dequeue();
+                max = Math.Max(max, signal);
+            }
+
+            Console.WriteLine($"Part 2: {max}");
+        }
+
+        //thanks to SO user Pengyang
+        //https://stackoverflow.com/a/10630026/1038840
+        //used under CC-BY-SA 4.0
+        private static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(e => !t.Contains(e)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
         }
     }
 }
