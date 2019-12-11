6263f3bdefccf3da9c6a2a4f6e0ce4b768bfe7db
Evan Cooper <coopere5@users.noreply.github.com>
2019-12-11 09:25:48 -0500

    add RunNext, public InputQueue

--- IntcodeComputer.cs
+++ IntcodeComputer.cs
@@ -8,12 +8,16 @@ namespace AdventUtils
     {
         private readonly IDictionary<long, long> Memory;
         private long RelativeBase;
-        private long CurrentPos;
+        private long InstructionPtr;
+
+        public Queue<long> InputQueue { get; set; }
 
         public IntcodeComputer()
         {
             RelativeBase = 0;
-            CurrentPos = 0;
+            InstructionPtr = 0;
+
+            InputQueue = new Queue<long>();
         }
 
         public IntcodeComputer(IEnumerable<long> intcode) : this()
@@ -29,158 +33,163 @@ namespace AdventUtils
 
         public long Run()
         {
-            long output;
-            return Run(out output);
+            return Run(out long output);
         }
 
         public long Run(out long output, params long[] inputParams)
         {
+            long rv = 0;
             RelativeBase = 0;
-            CurrentPos = 0;
-            output = -1;
-            var inputList = inputParams.ToList();
-            var sw = System.Diagnostics.Stopwatch.StartNew();
+            InstructionPtr = 0;
+            output = 0;
+            Array.ForEach(inputParams, e => InputQueue.Enqueue(e));
 
-            while (true)
+            bool running = true;
+            while (running)
             {
-                string fullOpcode = Memory[CurrentPos].ToString().PadLeft(5, '0');
-                long opcode = long.Parse(fullOpcode.Substring(3, 2));
-                long param1Mode = long.Parse(fullOpcode.Substring(2, 1));
-                long param2Mode = long.Parse(fullOpcode.Substring(1, 1));
-                long param3Mode = long.Parse(fullOpcode.Substring(0, 1));
+                rv = RunNext(ref output);
+                running = rv == long.MinValue;
+            }
 
-                long param1 = 0;
-                long param2 = 0;
-                long param3 = 0;
+            return rv;
+        }
 
-                long param1Key = 0;
-                long param2Key = 0;
-                long param3Key = 0;
+        public long RunNext(ref long output)
+        {
+            string fullOpcode = Memory[InstructionPtr].ToString().PadLeft(5, '0');
+            long opcode = long.Parse(fullOpcode.Substring(3, 2));
+            long param1Mode = long.Parse(fullOpcode.Substring(2, 1));
+            long param2Mode = long.Parse(fullOpcode.Substring(1, 1));
+            long param3Mode = long.Parse(fullOpcode.Substring(0, 1));
 
-                if (new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }.Contains(opcode))
+            long param1 = 0;
+            long param2 = 0;
+            long param3 = 0;
+
+            long param1Key = 0;
+            long param2Key = 0;
+            long param3Key = 0;
+
+            if (new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }.Contains(opcode))
+            {
+                if (!Memory.ContainsKey(InstructionPtr + 1)) Memory.Add(InstructionPtr + 1, 0);
+                switch (param1Mode)
                 {
-                    if (!Memory.ContainsKey(CurrentPos + 1)) Memory.Add(CurrentPos + 1, 0);
-                    switch (param1Mode)
-                    {
-                        case 0:
-                            param1Key = Memory[CurrentPos + 1];
-                            if (!Memory.ContainsKey(param1Key)) Memory.Add(param1Key, 0);
-                            param1 = Memory[param1Key];
-                            break;
-                        case 1:
-                            param1 = Memory[CurrentPos + 1];
-                            break;
-                        case 2:
-                            param1Key = RelativeBase + Memory[CurrentPos + 1];
-                            if (!Memory.ContainsKey(param1Key)) Memory.Add(param1Key, 0);
-                            param1 = Memory[param1Key];
-                            break;
-                    }
-                }
-
-                if (new long[] { 1, 2, 5, 6, 7, 8 }.Contains(opcode))
-                {
-                    if (!Memory.ContainsKey(CurrentPos + 2)) Memory.Add(CurrentPos + 2, 0);
-                    switch (param2Mode)
-                    {
-                        case 0:
-                            param2Key = Memory[CurrentPos + 2];
-                            if (!Memory.ContainsKey(param2Key)) Memory.Add(param2Key, 0);
-                            param2 = Memory[param2Key];
-                            break;
-                        case 1:
-                            param2 = Memory[CurrentPos + 2];
-                            break;
-                        case 2:
-                            param2Key = RelativeBase + Memory[CurrentPos + 2];
-                            if (!Memory.ContainsKey(param2Key)) Memory.Add(param2Key, 0);
-                            param2 = Memory[param2Key];
-                            break;
-                    }
-                }
-
-                if (new long[] { 1, 2, 7, 8 }.Contains(opcode))
-                {
-                    if (!Memory.ContainsKey(CurrentPos + 3)) Memory.Add(CurrentPos + 3, 0);
-                    switch (param3Mode)
-                    {
-                        case 0:
-                            param3Key = Memory[CurrentPos + 3];
-                            if (!Memory.ContainsKey(param3Key)) Memory.Add(param3Key, 0);
-                            param3 = Memory[param3Key];
-                            break;
-                        case 1:
-                            param3 = Memory[CurrentPos + 3];
-                            break;
-                        case 2:
-                            param3Key = RelativeBase + Memory[CurrentPos + 3];
-                            if (!Memory.ContainsKey(param3Key)) Memory.Add(param3Key, 0);
-                            param3 = Memory[param3Key];
-                            break;
-                    }
-                }
-
-                switch (opcode)
-                {
+                    case 0:
+                        param1Key = Memory[InstructionPtr + 1];
+                        if (!Memory.ContainsKey(param1Key)) Memory.Add(param1Key, 0);
+                        param1 = Memory[param1Key];
+                        break;
                     case 1:
-                        Memory[param3Key] = param1 + param2;
-                        CurrentPos += 4;
+                        param1 = Memory[InstructionPtr + 1];
                         break;
                     case 2:
-                        Memory[param3Key] = param1 * param2;
-                        CurrentPos += 4;
+                        param1Key = RelativeBase + Memory[InstructionPtr + 1];
+                        if (!Memory.ContainsKey(param1Key)) Memory.Add(param1Key, 0);
+                        param1 = Memory[param1Key];
                         break;
-                    case 3:
-                        Console.Write("Input: ");
-                        string input;
-                        if (inputList.Any())
-                        {
-                            input = inputList[0].ToString();
-                            Console.WriteLine(input);
-                            inputList.RemoveAt(0);
-                        }
-                        else
-                        {
-                            sw.Stop();
-                            input = Console.ReadLine();
-                            sw.Start();
-                        }
-                        Memory[param1Key] = int.Parse(input.Trim());
-                        CurrentPos += 2;
-                        break;
-                    case 4:
-                        Console.WriteLine($"Output: {param1}");
-                        output = param1;
-                        CurrentPos += 2;
-                        break;
-                    case 5:
-                        if (param1 != 0) CurrentPos = param2;
-                        else CurrentPos += 3;
-                        break;
-                    case 6:
-                        if (param1 == 0) CurrentPos = param2;
-                        else CurrentPos += 3;
-                        break;
-                    case 7:
-                        Memory[param3Key] = param1 < param2 ? 1 : 0;
-                        CurrentPos += 4;
-                        break;
-                    case 8:
-                        Memory[param3Key] = param1 == param2 ? 1 : 0;
-                        CurrentPos += 4;
-                        break;
-                    case 9:
-                        RelativeBase += param1;
-                        CurrentPos += 2;
-                        break;
-                    case 99:
-                        sw.Stop();
-                        System.Diagnostics.Debug.WriteLine(sw.Elapsed);
-                        return Memory[0];
-                    default:
-                        throw new Exception($"Unknown opcode: {opcode} at {CurrentPos}");
                 }
             }
+
+            if (new long[] { 1, 2, 5, 6, 7, 8 }.Contains(opcode))
+            {
+                if (!Memory.ContainsKey(InstructionPtr + 2)) Memory.Add(InstructionPtr + 2, 0);
+                switch (param2Mode)
+                {
+                    case 0:
+                        param2Key = Memory[InstructionPtr + 2];
+                        if (!Memory.ContainsKey(param2Key)) Memory.Add(param2Key, 0);
+                        param2 = Memory[param2Key];
+                        break;
+                    case 1:
+                        param2 = Memory[InstructionPtr + 2];
+                        break;
+                    case 2:
+                        param2Key = RelativeBase + Memory[InstructionPtr + 2];
+                        if (!Memory.ContainsKey(param2Key)) Memory.Add(param2Key, 0);
+                        param2 = Memory[param2Key];
+                        break;
+                }
+            }
+
+            if (new long[] { 1, 2, 7, 8 }.Contains(opcode))
+            {
+                if (!Memory.ContainsKey(InstructionPtr + 3)) Memory.Add(InstructionPtr + 3, 0);
+                switch (param3Mode)
+                {
+                    case 0:
+                        param3Key = Memory[InstructionPtr + 3];
+                        if (!Memory.ContainsKey(param3Key)) Memory.Add(param3Key, 0);
+                        param3 = Memory[param3Key];
+                        break;
+                    case 1:
+                        param3 = Memory[InstructionPtr + 3];
+                        break;
+                    case 2:
+                        param3Key = RelativeBase + Memory[InstructionPtr + 3];
+                        if (!Memory.ContainsKey(param3Key)) Memory.Add(param3Key, 0);
+                        param3 = Memory[param3Key];
+                        break;
+                }
+            }
+
+            switch (opcode)
+            {
+                case 1:
+                    Memory[param3Key] = param1 + param2;
+                    InstructionPtr += 4;
+                    break;
+                case 2:
+                    Memory[param3Key] = param1 * param2;
+                    InstructionPtr += 4;
+                    break;
+                case 3:
+                    Console.Write("Input: ");
+                    string input;
+                    if (InputQueue.Any())
+                    {
+                        input = InputQueue.Dequeue().ToString();
+                        Console.WriteLine(input);
+                    }
+                    else
+                    {
+                        input = Console.ReadLine();
+                    }
+                    Memory[param1Key] = int.Parse(input.Trim());
+                    InstructionPtr += 2;
+                    break;
+                case 4:
+                    Console.WriteLine($"Output: {param1}");
+                    output = param1;
+                    InstructionPtr += 2;
+                    break;
+                case 5:
+                    if (param1 != 0) InstructionPtr = param2;
+                    else InstructionPtr += 3;
+                    break;
+                case 6:
+                    if (param1 == 0) InstructionPtr = param2;
+                    else InstructionPtr += 3;
+                    break;
+                case 7:
+                    Memory[param3Key] = param1 < param2 ? 1 : 0;
+                    InstructionPtr += 4;
+                    break;
+                case 8:
+                    Memory[param3Key] = param1 == param2 ? 1 : 0;
+                    InstructionPtr += 4;
+                    break;
+                case 9:
+                    RelativeBase += param1;
+                    InstructionPtr += 2;
+                    break;
+                case 99:
+                    return Memory[0];
+                default:
+                    throw new Exception($"Unknown opcode: {opcode} at {InstructionPtr}");
+            }
+
+            return long.MinValue;
         }
     }
 }
