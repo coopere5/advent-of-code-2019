84b3b27c2708b9ba2b98449b5d7daee67d206eae
Evan Cooper <coopere5@users.noreply.github.com>
2019-12-19 15:43:05 -0500

    attempt part 2

--- Day 19/Program.cs
+++ Day 19/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -27,7 +28,7 @@ namespace Day19
             for (int y = 0; y < 50; y++)
             {
                 bool found = false;
-                int curX = lastX * 2;
+                int curX = lastX + 1;
                 for (int x = firstX; x < lastX; x++)
                 {
                     IntcodeComputer computer = new IntcodeComputer(input);
@@ -59,9 +60,77 @@ namespace Day19
 
             var input = File.ReadAllText("input.txt").Split(',').Select(long.Parse).ToArray();
 
+            int firstX = 0;
+            int lastX = 50;
+            int y = 0;
+            int columnCount = 0;
+
+            var screen = new HashSet<Point>();
+            bool running = true;
+            while(running)
+            {
+                columnCount = 0;
+                bool found = false;
+                int curX = lastX + 1;
+                for (int x = firstX; x < lastX; x++)
+                {
+                    IntcodeComputer computer = new IntcodeComputer(input);
+                    computer.Run(x, y);
+                    if (computer.OutputQueue.Dequeue() == 1)
+                    {
+                        if (!found)
+                        {
+                            firstX = x;
+                            if (lastX - firstX < 100)
+                            {
+                                curX = lastX + 2;
+                                break;
+                            }
+
+                            if (screen.Count(p => p.X == x) >= 100)
+                            {
+                                if (screen.Contains(new Point(x, y - 99)))
+                                {
+                                    Console.WriteLine($"Part 2: {x * 10000 + (y - 99)}");
+                                    running = false;
+                                }
+                            }
+                        }
+                        found = true;
+                        screen.Add(new Point(x, y));
+                        columnCount++;
+                    }
+                    else if (found)
+                    {
+                        curX = x + 1;
+                        break;
+                    }
+                }
+                lastX = curX;
+                y++;
+            }
 
             sw.Stop();
             Debug.WriteLine(sw.Elapsed);
+        }
+    }
+
+    public struct Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public static Point[] Range(Point start, int count) => (from x in Enumerable.Range(start.X, count) from y in Enumerable.Range(start.Y, count) select new Point(x, y)).ToArray();
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
         }
     }
 }
