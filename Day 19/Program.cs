9201dbdd35439296ce0b2916117ae3a2c25d6851
Evan Cooper <coopere5@users.noreply.github.com>
2019-12-19 15:50:34 -0500

    finish part 2

--- Day 19/Program.cs
+++ Day 19/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -61,76 +60,34 @@ namespace Day19
             var input = File.ReadAllText("input.txt").Split(',').Select(long.Parse).ToArray();
 
             int firstX = 0;
-            int lastX = 50;
-            int y = 0;
-            int columnCount = 0;
+            int y = 99;
 
-            var screen = new HashSet<Point>();
             bool running = true;
-            while(running)
+            while (running)
             {
-                columnCount = 0;
-                bool found = false;
-                int curX = lastX + 1;
-                for (int x = firstX; x < lastX; x++)
+                for (int x = firstX; x < 10000; x++)
                 {
                     IntcodeComputer computer = new IntcodeComputer(input);
                     computer.Run(x, y);
+
+                    if (computer.OutputQueue.Dequeue() != 1) continue;
+
+                    firstX = x;
+                    computer = new IntcodeComputer(input);
+                    computer.Run(x + 99, y - 99);
                     if (computer.OutputQueue.Dequeue() == 1)
                     {
-                        if (!found)
-                        {
-                            firstX = x;
-                            if (lastX - firstX < 100)
-                            {
-                                curX = lastX + 2;
-                                break;
-                            }
+                        Console.WriteLine($"Part 2: {x * 10000 + y - 99}");
+                        running = false;
+                    }
 
-                            if (screen.Count(p => p.X == x) >= 100)
-                            {
-                                if (screen.Contains(new Point(x, y - 99)))
-                                {
-                                    Console.WriteLine($"Part 2: {x * 10000 + (y - 99)}");
-                                    running = false;
-                                }
-                            }
-                        }
-                        found = true;
-                        screen.Add(new Point(x, y));
-                        columnCount++;
-                    }
-                    else if (found)
-                    {
-                        curX = x + 1;
-                        break;
-                    }
+                    break;
                 }
-                lastX = curX;
                 y++;
             }
 
             sw.Stop();
             Debug.WriteLine(sw.Elapsed);
-        }
-    }
-
-    public struct Point
-    {
-        public Point(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public int X { get; set; }
-        public int Y { get; set; }
-
-        public static Point[] Range(Point start, int count) => (from x in Enumerable.Range(start.X, count) from y in Enumerable.Range(start.Y, count) select new Point(x, y)).ToArray();
-
-        public override string ToString()
-        {
-            return $"({X},{Y})";
         }
     }
 }
