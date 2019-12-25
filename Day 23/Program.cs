b484c2c8f467897550f7b4a5532f977fc7db478d
coopere5 <coopere5@users.noreply.github.com>
2019-12-25 16:42:45 -0500

    start days 23 and 24

---
+++ Day 23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AdventUtils;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day23
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Part1();
+            Part2();
+            Console.ReadLine();
+        }
+
+        private static void Part1()
+        {
+            var sw = Stopwatch.StartNew();
+
+            sw.Stop();
+            Debug.WriteLine(sw.Elapsed);
+        }
+
+        private static void Part2()
+        {
+            var sw = Stopwatch.StartNew();
+
+            sw.Stop();
+            Debug.WriteLine(sw.Elapsed);
+        }
+    }
+}
