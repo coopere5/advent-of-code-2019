cf8c2f4c32797df6af1cf16acf1ec65b11dbaf74
Evan Cooper <coopere5@users.noreply.github.com>
2019-12-26 10:53:09 -0500

    finish part 1

--- Day 23/Program.cs
+++ Day 23/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using AdventUtils;
 using System.Diagnostics;
 using System.IO;
@@ -17,7 +17,41 @@ namespace Day23
 
         private static void Part1()
         {
-            var sw = Stopwatch.StartNew();
+            Stopwatch sw = Stopwatch.StartNew();
+
+            var input = File.ReadAllText("input.txt").Split(',').Select(long.Parse).ToArray();
+            var computers = new IntcodeComputer[50];
+            for (int i = 0; i < computers.Length; i++)
+            {
+                computers[i] = new IntcodeComputer(input);
+                computers[i].InputQueue.Enqueue(i);
+            }
+
+            bool running = true;
+            while (running)
+            {
+                foreach (IntcodeComputer computer in computers)
+                {
+                    if (computer.AwaitingInput()) computer.InputQueue.Enqueue(-1);
+                    computer.RunNext();
+                    if (computer.OutputQueue.Count == 3)
+                    {
+                        long address = computer.OutputQueue.Dequeue();
+                        long x = computer.OutputQueue.Dequeue();
+                        long y = computer.OutputQueue.Dequeue();
+
+                        if (address == 255)
+                        {
+                            Console.WriteLine($"Part 1: {y}");
+                            running = false;
+                            break;
+                        }
+
+                        computers[address].InputQueue.Enqueue(x);
+                        computers[address].InputQueue.Enqueue(y);
+                    }
+                }
+            }
 
             sw.Stop();
             Debug.WriteLine(sw.Elapsed);
@@ -25,7 +59,7 @@ namespace Day23
 
         private static void Part2()
         {
-            var sw = Stopwatch.StartNew();
+            Stopwatch sw = Stopwatch.StartNew();
 
             sw.Stop();
             Debug.WriteLine(sw.Elapsed);
